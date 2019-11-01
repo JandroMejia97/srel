d931813996c11e46532e7499f09d81c995b6b79e
JandroMejia97 <alejhrndz97@outlook.com>
2019-11-01 17:10:45 -0300

    Update settings.py

--- srel/settings.py
+++ srel/settings.py
@@ -51,10 +51,6 @@ INSTALLED_APPS = [
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.TokenAuthentication',
-        'rest_framework.authentication.BasicAuthentication',
-    ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
     ]
